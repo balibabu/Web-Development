6d6168c644800338a4a9406e1c7f21bbf0a89a8d
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2022-10-23 00:46:45 +0530

    javaScript

---
+++ javaScript/1_alert.js
@@ -0,0 +1,17 @@
+alert(2+3);
+alert('bali');
+typeof(true);
+var name=prompt('enter your name ?');
+alert(name.length);
+name.slice(2,5); //starting index, ending index , exclude ending index
+name.toUpperCase();
+name.toLowerCase();
+//precedence bodmas
+
+function greet(){
+    alert('hello boss');
+}
+console.log('hello');
+Math.floor(5/2); // -> 2
+Math.pow(2,3); // 8
+Math.round(5/2); // 3
---
+++ javaScript/collection.js
@@ -0,0 +1,3 @@
+var list=[1,2,3,4]
+Math.random() // gives 0-0.9999 upto 16 digits
+list.push(5);
